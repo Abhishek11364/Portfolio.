80782d8f4d1e46a6af49967d0d1a9008db0543a5
abhi maurya <98451783+Abhishek11364@users.noreply.github.com>
2024-01-28 12:46:08 +0000

    updated

--- lib/data.ts
+++ lib/data.ts
@@ -1,8 +1,10 @@
 import React from "react";
 
-import QueryPDF from "@/public/QueryPDF.png";
-import BhookhLagiHai from "@/public/Bhookh_Lagi_Hai.png";
-import Slima from "@/public/Slima.png";
+import Taste from "@/public/Tasty Treat.png";
+import Coin from "@/public/CoinChaser.png";
+import Project from "@/public/Project Manager.png";
+import Resume from "@/public/ResumeBuilder.png";
+
 import { Cloud, Laptop2 } from "lucide-react";
 
 export const links = [
@@ -34,32 +36,30 @@ export const projectsData = [
     description:
       "Developed a high-performance and responsive pizza ordering app, Payment gateway as Razopay",
     tags: ["React", "React-Router", "Redux-Toolkit", "Javascript", "EmailJs"],
-    imageUrl: BhookhLagiHai,
-    link: "https://pizza-delivery-app-594l.vercel.app",
+    imageUrl: Taste,
+    link: "https://tasty-treat-five.vercel.app/",
   },
   {
     title: "CoinChaser",
     description:
       "Developed a Crypocurrency price tracker app using CoinGecko API",
-    tags: ["React.js", "React-Router", "Chart.js", "Material ui","Firebase"],
-    imageUrl: QueryPDF,
+    tags: ["React.js", "React-Router", "Chart.js", "Material ui", "Firebase"],
+    imageUrl: Coin,
     link: "https://coin-chaser-32y4.vercel.app/",
   },
 
   {
     title: "ResumeForge",
-    description:
-      "Developed a Resume builder app using React and React-Router",
+    description: "Developed a Resume builder app using React and React-Router",
     tags: ["React", , "React-router"],
-    imageUrl: Slima,
+    imageUrl: Resume,
     link: "https://resume-builder-n6n8.vercel.app",
   },
   {
     title: "Projects Manager",
-    description:
-      "Developed a Project Manager app using React and React-Router",
+    description: "Developed a Project Manager app using React and React-Router",
     tags: ["React", , "React-router"],
-    imageUrl: Slima,
+    imageUrl: Project,
     link: "https://project-manager-dusky.vercel.app/",
   },
 ] as const;
@@ -70,8 +70,7 @@ export const skillsData = [
   "Tailwind",
   "Java",
   "JavaScript",
-  
-  
+
   "Firebase",
   "Git",
   "Redux Toolkit",
